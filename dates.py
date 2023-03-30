264f94e26f85ea09d5abd81912968ebbb8958320
Om Patil <97589129+omhpatil@users.noreply.github.com>
2023-03-31 00:01:23 +0530

    Delete dates.py

--- dates.py
+++
@@ -1,9 +0,0 @@
-# A date in Python is not a data type of its own, but we can import a module named datetime to work with dates as date objects.
-
-import datetime
-
-x = datetime.datetime.now()
-
-print(x.strftime("%d %b %Y"))
-print(x.strftime("%A"))
-print(x.strftime("%c"))
